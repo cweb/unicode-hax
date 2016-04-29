2db25b0d62ccb07a86c4f1dcc839d799d18c23d2
Chris Weber <chrisweber@live.com>
2016-04-29 14:27:00 -0700

    Add UTF-8 percent-encodings for each code point

--- UniHax/Fuzzer.cs
+++ UniHax/Fuzzer.cs
@@ -44,77 +44,95 @@ namespace UniHax
         /// <summary>
         /// The Byte Order Mark U+FEFF is a special character defining the byte order and endianess
         /// of text data.
+        /// UTF-8 percent encoding is %EF%BB%BF
         /// </summary>
         public static readonly string uBOM = "\uFEFF";
         /// <summary>
         /// The Right to Left Override U+202E defines special meaning to re-order the 
         /// display of text for right-to-left reading.
+        /// UTF-8 percent encoding is %E2%80%AE
         /// </summary>
         public static readonly string uRLO = "\u202E";
         /// <summary>
         /// Mongolian Vowel Separator U+180E is invisible and has the whitespace property.
+        /// UTF-8 percent encoding is %E1%A0%8E
         /// </summary>
         public static readonly string uMVS = "\u180E";
         /// <summary>
         /// Word Joiner U+2060 is an invisible zero-width character.
+        /// UTF-8 percent encoding is %E2%81%A0
         /// </summary>
         public static readonly string uWordJoiner = "\u2060";
         /// <summary>
         /// A reserved code point U+FEFE
+        /// UTF-8 percent encoding is %ef%bb%bf
         /// </summary>
         public static readonly string uReservedCodePoint = "\uFEFE";
         /// <summary>
         /// The code point U+FFFF is guaranteed to not be a Unicode character at all
+        /// UTF-8 percent encoding is %ef%bf%bf
         /// </summary>
         public static readonly string uNotACharacter = "\uFFFF";
         /// <summary>
         /// An unassigned code point U+0FED
+        /// UTF-8 percent encoding is %e0%bf%ad
         /// </summary>
         public static readonly string uUnassigned = "\u0FED";
         /// <summary>
         ///  An illegal low half-surrogate U+DEAD
+        /// UTF-8 percent encoding is %ed%ba%ad
         /// </summary>
         public static readonly string uDEAD = "\uDEAD";
         /// <summary>
         /// An illegal high half-surrogate U+DAAD
+        /// UTF-8 percent encoding is %ed%aa%ad
         /// </summary>
         public static readonly string uDAAD = "\uDAAD";
         /// <summary>
         /// A Private Use Area code point U+F8FF which Apple happens to use for its logo.
+        /// UTF-8 percent encoding is %EF%A3%BF
         /// </summary>
         public static readonly string uPrivate = "\uF8FF";
         /// <summary>
         /// U+FF0F FULLWIDTH SOLIDUS should normalize to / in a hostname
+        /// UTF-8 percent encoding is %F0%9D%9F%96
         /// </summary>
         public static readonly string uFullwidthSolidus = "\uFF0F";
         /// <summary>
         /// Code point with a numerical mapping and value U+1D7D6 MATHEMATICAL BOLD DIGIT EIGHT
+        /// UTF-8 percent encoding is 
         /// </summary>
         public static readonly string uBoldEight = char.ConvertFromUtf32(0x1D7D6);
         /// <summary>
         /// IDNA2003/2008 Deviant - U+00DF normalizes to "ss" during IDNA2003's mapping phase,
         /// different from its IDNA2008 mapping.
         /// See http://www.unicode.org/reports/tr46/
+        /// UTF-8 percent encoding is %C3%9F
         /// </summary>
         public static readonly string uIdnaSs = "\u00DF";
         /// <summary>
         /// U+FDFD expands by 11x (UTF-8) and 18x (UTF-16) under NFKC/NFKC
+        /// UTF-8 percent encoding is %EF%B7%BA
         /// </summary>
         public static readonly string uFDFA = "\uFDFA";
         /// <summary>
         /// U+0390 expands by 3x (UTF-8) under NFD
+        /// UTF-8 percent encoding is %CE%90
         /// </summary>
         public static readonly string u0390 = "\u0390";
         /// <summary>
         /// U+1F82 expands by 4x (UTF-16) under NFD
+        /// UTF-8 percent encoding is %E1%BE%82
         /// </summary>
         public static readonly string u1F82 = "\u1F82";
         /// <summary>
         /// U+FB2C expands by 3x (UTF-16) under NFC
+        /// UTF-8 percent encoding is %EF%AC%AC
         /// </summary>
         public static readonly string uFB2C = "\uFB2C";
         /// <summary>
         /// U+1D160 expands by 3x (UTF-8) under NFC
+        /// UTF-8 percent encoding is %F0%9D%85%A0
         /// </summary>
         public static readonly string u1D160 = char.ConvertFromUtf32(0x1D160);
 
@@ -197,6 +215,7 @@ namespace UniHax
         /// <summary>
         /// Return a UTF32 byte encoding for an illegal code point value U+1FFFFF.  
         /// Note that Unicode 6.0 supports only up to U+10FFFF.
+        /// UTF-8 percent encoding for something out of range is %F4%8F%BF%BE
         /// </summary>
         /// <returns>A raw byte array because .NET will not allow illegal code points in the System.String class.</returns>
         public byte[] OutOfRangeCodePointAsUtf32BE()
